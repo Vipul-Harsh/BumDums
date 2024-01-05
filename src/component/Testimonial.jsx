963edd723d54fa2aaec81cec191eb026159f1c5d
Darpan <darpan9460@gmail.com>
2024-01-05 22:47:47 +0530

    Added testimonial data

--- src/component/Testimonial.jsx
+++ src/component/Testimonial.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Slider from 'react-slick';
+import { TestimonialsData } from '../data/testimonials';
 const Testimonial = () => {
     const settingsTestimonial = {
         dots: true,
@@ -10,261 +11,73 @@ const Testimonial = () => {
         autoplay: true,
         autoplaySpeed: 2000,
     };
-  return (
-    <section className="testimonial testimonial2 padding-bottom-120 padding-top-110">
-    <div className="container">
-        <div className="testi-shapes">
-            <span className="ts-1">
-                <img src="assets/images/shapes/triple.png" alt="" />
-            </span>
-            <span className="ts-2 item-bounce">
-                <img src="assets/images/shapes/35.png" alt="" />
-            </span>
-            <span className="ts-3 item-animateTwo">
-                <img src="assets/images/shapes/26.png" alt="" />
-            </span>
-        </div>
-        <div className="common-title-area text-center padding-bottom-50 wow fadeInUp">
-            <h3>testimonial</h3>
-            <h2>
-                client <span>feedback</span>
-            </h2>
-        </div>
-        <div className="testimonial-active">
-            <Slider {...settingsTestimonial}>
-                <div className="single-testimonial">
-                    <div className="testi-top">
-                        <div className="tin-shapes">
-                            <span className="tsin-1">
-                                <img src="assets/images/shapes/33.png" alt="" />
-                            </span>
-                        </div>
-                        <div className="testi-img">
-                            <img src="assets/images/testimonial/testi-1.png" alt="" />
-                        </div>
-                        <div className="testi-meta">
-                            <h6>Christ Deo</h6>
-                            <p>CEO A4Tech Ltd.</p>
-                            <div className="testi-rating">
-                                <span>
-                                    <i className="fas fa-star" />
-                                </span>
-                                <span>
-                                    <i className="fas fa-star" />
-                                </span>
-                                <span>
-                                    <i className="fas fa-star" />
-                                </span>
-                                <span>
-                                    <i className="fas fa-star" />
-                                </span>
-                                <span>
-                                    <i className="fas fa-star" />
-                                </span>
-                            </div>
-                        </div>
-                    </div>
-                    <p>
-                        Food Khan is a gret Restaurant from the University of Texas at
-                        Austin has been researching the benefits of frequent testing and the
-                        feedback leads to. He explains that in the history of the study.
-                    </p>
+    return (
+        <section className="testimonial testimonial2 padding-bottom-120 padding-top-110">
+            <div className="container">
+                <div className="testi-shapes">
+                    <span className="ts-1">
+                        <img src="assets/images/shapes/triple.png" alt="" />
+                    </span>
+                    <span className="ts-2 item-bounce">
+                        <img src="assets/images/shapes/35.png" alt="" />
+                    </span>
+                    <span className="ts-3 item-animateTwo">
+                        <img src="assets/images/shapes/26.png" alt="" />
+                    </span>
                 </div>
-                <div className="single-testimonial">
-                    <div className="testi-top">
-                        <div className="tin-shapes">
-                            <span className="tsin-1">
-                                <img src="assets/images/shapes/33.png" alt="" />
-                            </span>
-                        </div>
-                        <div className="testi-img">
-                            <img src="assets/images/testimonial/testi-2.png" alt="" />
-                        </div>
-                        <div className="testi-meta">
-                            <h6>Christ Deo</h6>
-                            <p>CEO A4Tech Ltd.</p>
-                            <div className="testi-rating">
-                                <span>
-                                    <i className="fas fa-star" />
-                                </span>
-                                <span>
-                                    <i className="fas fa-star" />
-                                </span>
-                                <span>
-                                    <i className="fas fa-star" />
-                                </span>
-                                <span>
-                                    <i className="fas fa-star" />
-                                </span>
-                                <span>
-                                    <i className="fas fa-star" />
-                                </span>
-                            </div>
-                        </div>
-                    </div>
-                    <p>
-                        Food Khan is a gret Restaurant from the University of Texas at
-                        Austin has been researching the benefits of frequent testing and the
-                        feedback leads to. He explains that in the history of the study.
-                    </p>
+                <div className="common-title-area text-center padding-bottom-50 wow fadeInUp">
+                    <h3>testimonial</h3>
+                    <h2>
+                        client <span>feedback</span>
+                    </h2>
                 </div>
-                <div className="single-testimonial">
-                    <div className="testi-top">
-                        <div className="tin-shapes">
-                            <span className="tsin-1">
-                                <img src="assets/images/shapes/33.png" alt="" />
-                            </span>
-                        </div>
-                        <div className="testi-img">
-                            <img src="assets/images/testimonial/testi-1.png" alt="" />
-                        </div>
-                        <div className="testi-meta">
-                            <h6>Christ Deo</h6>
-                            <p>CEO A4Tech Ltd.</p>
-                            <div className="testi-rating">
-                                <span>
-                                    <i className="fas fa-star" />
-                                </span>
-                                <span>
-                                    <i className="fas fa-star" />
-                                </span>
-                                <span>
-                                    <i className="fas fa-star" />
-                                </span>
-                                <span>
-                                    <i className="fas fa-star" />
-                                </span>
-                                <span>
-                                    <i className="fas fa-star" />
-                                </span>
-                            </div>
-                        </div>
-                    </div>
-                    <p>
-                        Food Khan is a gret Restaurant from the University of Texas at
-                        Austin has been researching the benefits of frequent testing and the
-                        feedback leads to. He explains that in the history of the study.
-                    </p>
+                <div className="testimonial-active">
+                    <Slider {...settingsTestimonial}>
+                        {TestimonialsData.map(testimonial => {
+                            const starIcons = [];
+                            for (let i = 0; i < testimonial?.stars; i++) {
+                                starIcons.push(
+                                    <span key={i}>
+                                        <i className="fas fa-star" />
+                                    </span>
+                                );
+                            }
+                            return <>
+                                <div className="single-testimonial">
+                                    <div className="testi-top">
+                                        <div className="tin-shapes">
+                                            <span className="tsin-1">
+                                                <img src="/assets/images/shapes/33.png" alt="" />
+                                            </span>
+                                        </div>
+                                        <div className="testi-img">
+                                            <img src={testimonial.image} alt="" />
+                                        </div>
+                                        <div className="testi-meta">
+                                            <h6>{testimonial.name}</h6>
+                                            <p>{testimonial.role}</p>
+                                            <div className="testi-rating">
+                                                {starIcons.map(star =>
+                                                    <>
+                                                        <span>
+                                                            <i className="fas fa-star" />
+                                                        </span>
+                                                    </>
+                                                )}
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <p>
+                                        {testimonial.description}
+                                    </p>
+                                </div>
+                            </>
+                        })}
+                    </Slider>
                 </div>
-                <div className="single-testimonial">
-                    <div className="testi-top">
-                        <div className="tin-shapes">
-                            <span className="tsin-1">
-                                <img src="assets/images/shapes/33.png" alt="" />
-                            </span>
-                        </div>
-                        <div className="testi-img">
-                            <img src="assets/images/testimonial/testi-2.png" alt="" />
-                        </div>
-                        <div className="testi-meta">
-                            <h6>Christ Deo</h6>
-                            <p>CEO A4Tech Ltd.</p>
-                            <div className="testi-rating">
-                                <span>
-                                    <i className="fas fa-star" />
-                                </span>
-                                <span>
-                                    <i className="fas fa-star" />
-                                </span>
-                                <span>
-                                    <i className="fas fa-star" />
-                                </span>
-                                <span>
-                                    <i className="fas fa-star" />
-                                </span>
-                                <span>
-                                    <i className="fas fa-star" />
-                                </span>
-                            </div>
-                        </div>
-                    </div>
-                    <p>
-                        Food Khan is a gret Restaurant from the University of Texas at
-                        Austin has been researching the benefits of frequent testing and the
-                        feedback leads to. He explains that in the history of the study.
-                    </p>
-                </div>
-                <div className="single-testimonial">
-                    <div className="testi-top">
-                        <div className="tin-shapes">
-                            <span className="tsin-1">
-                                <img src="assets/images/shapes/33.png" alt="" />
-                            </span>
-                        </div>
-                        <div className="testi-img">
-                            <img src="assets/images/testimonial/testi-1.png" alt="" />
-                        </div>
-                        <div className="testi-meta">
-                            <h6>Christ Deo</h6>
-                            <p>CEO A4Tech Ltd.</p>
-                            <div className="testi-rating">
-                                <span>
-                                    <i className="fas fa-star" />
-                                </span>
-                                <span>
-                                    <i className="fas fa-star" />
-                                </span>
-                                <span>
-                                    <i className="fas fa-star" />
-                                </span>
-                                <span>
-                                    <i className="fas fa-star" />
-                                </span>
-                                <span>
-                                    <i className="fas fa-star" />
-                                </span>
-                            </div>
-                        </div>
-                    </div>
-                    <p>
-                        Food Khan is a gret Restaurant from the University of Texas at
-                        Austin has been researching the benefits of frequent testing and the
-                        feedback leads to. He explains that in the history of the study.
-                    </p>
-                </div>
-                <div className="single-testimonial">
-                    <div className="testi-top">
-                        <div className="tin-shapes">
-                            <span className="tsin-1">
-                                <img src="assets/images/shapes/33.png" alt="" />
-                            </span>
-                        </div>
-                        <div className="testi-img">
-                            <img src="assets/images/testimonial/testi-2.png" alt="" />
-                        </div>
-                        <div className="testi-meta">
-                            <h6>Christ Deo</h6>
-                            <p>CEO A4Tech Ltd.</p>
-                            <div className="testi-rating">
-                                <span>
-                                    <i className="fas fa-star" />
-                                </span>
-                                <span>
-                                    <i className="fas fa-star" />
-                                </span>
-                                <span>
-                                    <i className="fas fa-star" />
-                                </span>
-                                <span>
-                                    <i className="fas fa-star" />
-                                </span>
-                                <span>
-                                    <i className="fas fa-star" />
-                                </span>
-                            </div>
-                        </div>
-                    </div>
-                    <p>
-                        Food Khan is a gret Restaurant from the University of Texas at
-                        Austin has been researching the benefits of frequent testing and the
-                        feedback leads to. He explains that in the history of the study.
-                    </p>
-                </div>
-            </Slider>
-        </div>
-    </div>
-</section>
-  )
+            </div>
+        </section>
+    )
 }
 
 export default Testimonial
